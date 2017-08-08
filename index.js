31c70fd042f8dc0f347a219bc56d9289d776479b
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-08-08 17:23:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+	let counter = 0
+	
+	function addBump(){
+		counter += 1
+		}
+	 function getBumps(){
+		return counter
+		}
+
+	return {addBump,
+ 			getBumps};
+}
+
+function createAnimal(animalType){
+	function addDevice (deadlyDevice){
+		 let object = {animalType: `${animalType}`, deadlyDevice: `${deadlyDevice}`}
+		 return object 
+	}
+	return addDevice;
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = createAnimal('Shark')('Cannon')
